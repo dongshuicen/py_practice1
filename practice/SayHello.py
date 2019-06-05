464c72bde3d9bbe4e84a1199d645246a03a54870
dongshuicen <dong12()@>
2019-06-05 23:28:53 +0800

    init

---
+++ practice/SayHello.py
@@ -0,0 +1,19 @@
+def hello(name, age):
+    print("name:" + name)
+    print("age:" + str(age))
+    return
+
+
+hello("dongshuicen", 30)
+
+array = [1,2,3,4,5,6,7,8]
+print(array[0])
+array[1]=10
+print(array[1])
+#yuanzu=[]
+lambdatest='lambdatest'
+lambdatest='bbc'
+print(lambdatest)
+#if __name__ == '__main__':
+#    hello("dong", 30)
+#from mailbox import *
